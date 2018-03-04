91d2538de37ca9eeddc1a0620aae08e63f8c06c2
Bhargav Teja <teja.bhargav@gmail.com>
2018-03-04 03:25:06 -0600

    Fetch URL from Config File

--- yelp_restaurants_scrape.py
+++ yelp_restaurants_scrape.py
@@ -1,8 +1,25 @@
 from bs4 import BeautifulSoup
+import yaml
+
+
+config_file_path = "config.yml"
+
+
+def url_patter(zipcode):
+    with open(config_file_path) as paramStream:
+        param_dict = yaml.load(paramStream)
+        return param_dict["parameters"]["main_url_pattern"].format(zip_code=zipcode)
+
+
+def configured_zip():
+    with open(config_file_path) as paramStream:
+        param_dict = yaml.load(paramStream)
+        return param_dict["parameters"]["zipcode"]
+
 
 import requests
 
-url = "https://www.yelp.com/search?find_desc=&find_loc=Chicago%2C+IL+60610&ns=1"
+url = url_patter(configured_zip())
 r = requests.get(url)
 html_content = r.text
 soup = BeautifulSoup(html_content, "html.parser")
