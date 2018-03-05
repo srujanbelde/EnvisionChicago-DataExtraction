0411a3d81f8eca04707a7ff50eeb6b6579d9c55c
Bhargav Teja <teja.bhargav@gmail.com>
2018-03-05 15:53:26 -0600

    API defined to extract all the reviews for a restaurant

--- yelp_reviews_scrape.py
+++ yelp_reviews_scrape.py
@@ -2,82 +2,99 @@ import urllib.request
 from bs4 import BeautifulSoup
 import yaml
 import requests
+import json
+
 
 config_file_path = "config.yml"
 
-
-def url_patter(zipcode):
-    with open(config_file_path) as paramStream:
-        param_dict = yaml.load(paramStream)
-        return param_dict["parameters"]["main_url_pattern"].format(zip_code=zipcode)
-
-
-def configured_zip():
-    with open(config_file_path) as paramStream:
-        param_dict = yaml.load(paramStream)
-        return param_dict["parameters"]["zipcode"]
+funny_count_identifier = "ybtn ybtn--small funny js-analytics-click"
+useful_count_identifier = "ybtn ybtn--small useful js-analytics-click"
+cool_count_identifier = "ybtn ybtn--small cool js-analytics-click"
+rating_identifier = "i-stars"
+total_reviews_identifier = "ylist ylist-bordered reviews"
+username_identifier = "user-display-name js-analytics-click"
+userid_identifier = "review review--with-sidebar"
 
 
-dict = []
+def formatted_id(userid):
+    fid = userid.replace("user_id:", "")
+    return fid.strip()
 
-def main():
+
+def formatted_rating(rating):
+    frating = rating.replace("star rating", "")
+    return frating.strip()
+
+
+def formatted_count(count):
+    fcount = 0
+    if count.strip() == '':
+        fcount = 0
+    else:
+        fcount = int(count.strip())
+
+    return fcount
+
+
+def get_reviews_for_restaurant(restaurant_url):
+
+    print("\nExtracting Reviews....\n")
 
     rev_data_list = []
-
-    url = "https://www.yelp.com/biz/girl-and-the-goat-chicago"
+    url = restaurant_url
     r = requests.get(url)
     html_content = r.text
     soup = BeautifulSoup(html_content, "html.parser")
 
-    class1 ="ylist ylist-bordered reviews"
-    lis2 = soup.find_all("ul", class_= class1)[0]
+    rev_count_tag = soup.find_all("span", attrs={'itemprop':'reviewCount'})[0]
+    rev_count = int(rev_count_tag.text)
+    rev_page_count = int(rev_count / 20) - 1
 
-    lis = lis2.find_all("li", recursive=False)
-    print(len(lis))
+    for i in range(0,rev_page_count):
 
-    #lis = soup.find_all('ul', attrs={'class:ylist ylist-bordered reviews'})
-    for li in lis:
+        ul_html = soup.find_all("ul", class_=total_reviews_identifier)[0]
+        li_list = ul_html.find_all("li", recursive=False)
 
-        new_review_dict = {}
+        for li in li_list:
 
-        if len(li.find_all("div", class_="review review--with-sidebar")) != 0:
-            rating = li.find_all("div", class_="review review--with-sidebar")[0]
-            new_review_dict["user_id"] = rating.attrs["data-signup-object"]
-            print(rating.attrs["data-signup-object"])
+            new_review_dict = {}
 
-        if len(li.find_all("a", class_="user-display-name js-analytics-click")) != 0:
-            rating = li.find_all("a", class_="user-display-name js-analytics-click")[0]
-            new_review_dict["user_name"] = rating.text
-            print(rating.text)
+            if len(li.find_all("div", class_=userid_identifier)) != 0:
+                userid_tag = li.find_all("div", class_=userid_identifier)[0]
+                new_review_dict["user_id"] = formatted_id(userid_tag.attrs["data-signup-object"])
 
-        if len(li.find_all("div", class_ = "i-stars")) != 0:
-            rating = li.find_all("div", class_ = "i-stars")
-            new_review_dict["rating"] = rating[0].attrs["title"]
-            print(rating[0].attrs["title"])
+            if len(li.find_all("a", class_=username_identifier)) != 0:
+                username_tag = li.find_all("a", class_=username_identifier)[0]
+                new_review_dict["user_name"] = username_tag.text
 
-        if len(li.find_all("p")) != 0:
-            rating = li.find_all("p")
-            new_review_dict["review"] = rating[0].text
-            print(rating[0].text)
+            if len(li.find_all("div", class_ = rating_identifier)) != 0:
+                rating_tag = li.find_all("div", class_ = rating_identifier)[0]
+                new_review_dict["rating"] = formatted_rating(rating_tag.attrs["title"])
 
-        if len(li.find_all("a", class_="ybtn ybtn--small useful js-analytics-click")) != 0:
-            child = li.find_all("a", class_="ybtn ybtn--small useful js-analytics-click")[0]
-            new_review_dict["useful_votes"] = child.find_all("span",class_="count")[0].text
-            print(child.find_all("span",class_="count")[0].text)
+            if len(li.find_all("p")) != 0:
+                feedback_tag = li.find_all("p")[0]
+                new_review_dict["review"] = feedback_tag.text
 
-        if len(li.find_all("a", class_="ybtn ybtn--small funny js-analytics-click")) != 0:
-            child = li.find_all("a", class_="ybtn ybtn--small funny js-analytics-click")[0]
-            new_review_dict["funny_votes"] = child.find_all("span", class_="count")[0].text
-            print(child.find_all("span", class_="count")[0].text)
+            if len(li.find_all("a", class_=useful_count_identifier)) != 0:
+                useful_count_tree = li.find_all("a", class_=useful_count_identifier)[0]
+                new_review_dict["useful_votes"] = formatted_count(useful_count_tree.find_all("span", class_="count")[0].text)
 
-        if len(li.find_all("a", class_="ybtn ybtn--small cool js-analytics-click")) != 0:
-            child = li.find_all("a", class_="ybtn ybtn--small cool js-analytics-click")[0]
-            new_review_dict["cool_votes"] = child.find_all("span", class_="count")[0].text
-            print(child.find_all("span", class_="count")[0].text)
+            if len(li.find_all("a", class_=funny_count_identifier)) != 0:
+                funny_count_tree = li.find_all("a", class_=funny_count_identifier)[0]
+                new_review_dict["funny_votes"] = formatted_count(funny_count_tree.find_all("span", class_="count")[0].text)
 
-        rev_data_list.append(new_review_dict)
+            if len(li.find_all("a", class_=cool_count_identifier)) != 0:
+                cool_count_tree = li.find_all("a", class_=cool_count_identifier)[0]
+                new_review_dict["cool_votes"] = formatted_count(cool_count_tree.find_all("span", class_="count")[0].text)
 
-    return rev_data_list
+            rev_data_list.append(new_review_dict)
+
+        percent = int((i * 100)/rev_page_count)
+        print('\r[{0}] {1}%'.format('#' * percent, percent), end="")
+
+    print('\r[{0}] {1}%'.format('#' * 100, 100))
+    print("\nReviews successfully extracted for {0}".format(restaurant_url))
+    return rev_data_list[1:]
 
 
-print(main())
+print("{0} {1}".format("\n\n",get_reviews_for_restaurant("https://www.yelp.com/biz/meli-cafe-and-juice-bar-chicago")))
