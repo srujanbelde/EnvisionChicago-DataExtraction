d4a0eb8446cff0cd67de73642a21930fad7e75ea
Bhargav Teja <teja.bhargav@gmail.com>
2018-03-07 23:03:32 -0600

    Code for author.csv

--- yelp_reviews_scrape.py
+++ yelp_reviews_scrape.py
@@ -14,7 +14,13 @@ rating_identifier = "i-stars"
 total_reviews_identifier = "ylist ylist-bordered reviews"
 username_identifier = "user-display-name js-analytics-click"
 userid_identifier = "review review--with-sidebar"
+location_identifier = "user-location responsive-hidden-small"
+friends_count_identifier = "friend-count responsive-small-display-inline-block"
+review_count_identifier = "review-count responsive-small-display-inline-block"
+photo_count_identifier = "photo-count responsive-small-display-inline-block"
 
+rev_data_list = []
+author_data_list = []
 
 def formatted_id(userid):
     fid = userid.replace("user_id:", "")
@@ -40,7 +46,6 @@ def get_reviews_for_restaurant(restaurant_url):
 
     print("\nExtracting Reviews....\n")
 
-    rev_data_list = []
     url = restaurant_url
     r = requests.get(url)
     html_content = r.text
@@ -61,38 +66,8 @@ def get_reviews_for_restaurant(restaurant_url):
         li_list = ul_html.find_all("li", recursive=False)
 
         for li in li_list:
-
-            new_review_dict = {}
-
-            if len(li.find_all("div", class_=userid_identifier)) != 0:
-                userid_tag = li.find_all("div", class_=userid_identifier)[0]
-                new_review_dict["user_id"] = formatted_id(userid_tag.attrs["data-signup-object"])
-
-            if len(li.find_all("a", class_=username_identifier)) != 0:
-                username_tag = li.find_all("a", class_=username_identifier)[0]
-                new_review_dict["user_name"] = username_tag.text
-
-            if len(li.find_all("div", class_ = rating_identifier)) != 0:
-                rating_tag = li.find_all("div", class_ = rating_identifier)[0]
-                new_review_dict["rating"] = formatted_rating(rating_tag.attrs["title"])
-
-            if len(li.find_all("p")) != 0:
-                feedback_tag = li.find_all("p")[0]
-                new_review_dict["review"] = feedback_tag.text
-
-            if len(li.find_all("a", class_=useful_count_identifier)) != 0:
-                useful_count_tree = li.find_all("a", class_=useful_count_identifier)[0]
-                new_review_dict["useful_votes"] = formatted_count(useful_count_tree.find_all("span", class_="count")[0].text)
-
-            if len(li.find_all("a", class_=funny_count_identifier)) != 0:
-                funny_count_tree = li.find_all("a", class_=funny_count_identifier)[0]
-                new_review_dict["funny_votes"] = formatted_count(funny_count_tree.find_all("span", class_="count")[0].text)
-
-            if len(li.find_all("a", class_=cool_count_identifier)) != 0:
-                cool_count_tree = li.find_all("a", class_=cool_count_identifier)[0]
-                new_review_dict["cool_votes"] = formatted_count(cool_count_tree.find_all("span", class_="count")[0].text)
-
-            rev_data_list.append(new_review_dict)
+            generate_review_list(li)
+            generate_author_list(li)
 
         percent = int((i * 100)/rev_page_count)
         print('\r[{0}] {1}%'.format('#' * percent, percent), end="")
@@ -102,4 +77,75 @@ def get_reviews_for_restaurant(restaurant_url):
     return rev_data_list[1:]
 
 
-print("{0} {1}".format("\n\n",get_reviews_for_restaurant("https://www.yelp.com/biz/meli-cafe-and-juice-bar-chicago")))
+def generate_review_list(li):
+    new_review_dict = {}
+
+    if len(li.find_all("div", class_=userid_identifier)) != 0:
+        userid_tag = li.find_all("div", class_=userid_identifier)[0]
+        new_review_dict["user_id"] = formatted_id(userid_tag.attrs["data-signup-object"])
+
+    if len(li.find_all("a", class_=username_identifier)) != 0:
+        username_tag = li.find_all("a", class_=username_identifier)[0]
+        new_review_dict["user_name"] = username_tag.text
+
+    if len(li.find_all("div", class_=rating_identifier)) != 0:
+        rating_tag = li.find_all("div", class_=rating_identifier)[0]
+        new_review_dict["rating"] = formatted_rating(rating_tag.attrs["title"])
+
+    if len(li.find_all("p")) != 0:
+        feedback_tag = li.find_all("p")[0]
+        new_review_dict["review"] = feedback_tag.text
+
+    if len(li.find_all("a", class_=useful_count_identifier)) != 0:
+        useful_count_tree = li.find_all("a", class_=useful_count_identifier)[0]
+        new_review_dict["useful_votes"] = formatted_count(useful_count_tree.find_all("span", class_="count")[0].text)
+
+    if len(li.find_all("a", class_=funny_count_identifier)) != 0:
+        funny_count_tree = li.find_all("a", class_=funny_count_identifier)[0]
+        new_review_dict["funny_votes"] = formatted_count(funny_count_tree.find_all("span", class_="count")[0].text)
+
+    if len(li.find_all("a", class_=cool_count_identifier)) != 0:
+        cool_count_tree = li.find_all("a", class_=cool_count_identifier)[0]
+        new_review_dict["cool_votes"] = formatted_count(cool_count_tree.find_all("span", class_="count")[0].text)
+
+    rev_data_list.append(new_review_dict)
+
+
+def generate_author_list(li):
+
+    new_author_list = []
+    if len(li.find_all("div", class_=userid_identifier)) != 0:
+        userid_tag = li.find_all("div", class_=userid_identifier)[0]
+        new_author_list.append(formatted_id(userid_tag.attrs["data-signup-object"]))
+
+    if len(li.find_all("a", class_=username_identifier)) != 0:
+        username_tag = li.find_all("a", class_=username_identifier)[0]
+        new_author_list.append(username_tag.text)
+
+    if len(li.find_all("li", class_=location_identifier)) != 0:
+        location_tag = li.find_all("li", class_=location_identifier)[0]
+        new_author_list.append(location_tag.text.strip())
+
+    if len(li.find_all("li", class_=friends_count_identifier)) != 0:
+        friend_count_tag = li.find_all("li", class_=friends_count_identifier)[0]
+        new_author_list.append(friend_count_tag.text.strip().split(" ")[0])
+
+    if len(li.find_all("li", class_=review_count_identifier)) != 0:
+        review_count_tag = li.find_all("li", class_=review_count_identifier)[0]
+        new_author_list.append(review_count_tag.text.strip().split(" ")[0])
+
+    if len(li.find_all("li", class_=photo_count_identifier)) != 0:
+        photo_count_tag = li.find_all("li", class_=photo_count_identifier)[0]
+        new_author_list.append(photo_count_tag.text.strip().split(" ")[0])
+
+    author_data_list.append(new_author_list)
+
+
+print("{0} {1}".format("\n\n", get_reviews_for_restaurant("https://www.yelp.com/biz/meli-cafe-and-juice-bar-chicago")))
+
+
+
+
+
+
+
