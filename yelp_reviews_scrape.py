310caa0fd09bd2cc06b337ec97c68e1a88be0eb4
Bhargav Teja <teja.bhargav@gmail.com>
2018-03-08 16:23:39 -0600

    Added CSV generation with appropriate attributes

--- yelp_reviews_scrape.py
+++ yelp_reviews_scrape.py
@@ -3,6 +3,9 @@ from bs4 import BeautifulSoup
 import yaml
 import requests
 import json
+import csv
+import time
+
 
 config_file_path = "config.yml"
 
@@ -24,6 +27,11 @@ category_identifier = "category-str-list"
 hours_identifier = "table table-simple hours-table"
 price_range_identifier = "business-attribute price-range"
 website_identifier = "biz-website js-biz-website js-add-url-tagging"
+date_identifier = "rating-qualifier"
+
+
+author_csv_path = "./author.csv"
+review_csv_path = "./review.csv"
 
 rev_data_list = []
 author_data_list = []
@@ -105,17 +113,16 @@ def get_reviews_for_restaurant(restaurant_url):
     html_content = r.text
     soup = BeautifulSoup(html_content, "html.parser")
 
-
+    rev_count_tag = soup.find_all("span", attrs={'itemprop': 'reviewCount'})[0]
+    rev_count = int(rev_count_tag.text)
+    rev_page_count = int(rev_count / 20) - 1
+    
     restaurant_csv = {}
     restaurant_name = soup.find('h1', attrs={'class': business_name_identifier})
 
     address_tag = soup.find('address').text.strip()
     address = address_tag.replace('Chicago', ', Chicago')
     address += " Neighborhood: " + soup.find('span', attrs={'class': neighborhood_identifier}).text.strip()
-
-    rev_count_tag = soup.find_all("span", attrs={'itemprop': 'reviewCount'})[0]
-    rev_count = int(rev_count_tag.text)
-    rev_page_count = int(rev_count / 20) - 1
 
     rating = soup.find('meta', attrs={'itemprop': 'ratingValue'})
 
@@ -136,8 +143,6 @@ def get_reviews_for_restaurant(restaurant_url):
     restaurant_csv['WheelchairAccessible'] = "No"
     div_biz_info = soup.find_all('div', class_='ywidget')
     restaurant_csv = get_more_biz_info(div_biz_info, restaurant_csv)
-
-
 
     price_range = soup.find('span', class_=price_range_identifier)
     website_span = soup.find('span', class_=website_identifier)
@@ -172,6 +177,7 @@ def get_reviews_for_restaurant(restaurant_url):
 
         percent = int((i * 100) / rev_page_count)
         print('\r[{0}] {1}%'.format('#' * percent, percent), end="")
+        time.sleep(5)
 
     print('\r[{0}] {1}%'.format('#' * 100, 100))
     print("\nReviews successfully extracted for {0}".format(restaurant_url))
@@ -179,40 +185,48 @@ def get_reviews_for_restaurant(restaurant_url):
 
 
 def generate_review_list(li):
-    new_review_dict = {}
+    new_review_list = []
+
+    if len(li.find_all("div", class_=userid_identifier)) != 0:
+        revid_tag = li.find_all("div", class_=userid_identifier)[0]
+        new_review_list.append(formatted_id(revid_tag.attrs["data-review-id"]))
+
+    #TODO: Business ID extraction
 
     if len(li.find_all("div", class_=userid_identifier)) != 0:
         userid_tag = li.find_all("div", class_=userid_identifier)[0]
-        new_review_dict["user_id"] = formatted_id(userid_tag.attrs["data-signup-object"])
+        #new_review_dict["user_id"] = formatted_id(userid_tag.attrs["data-signup-object"])
+        new_review_list.append(formatted_id(userid_tag.attrs["data-signup-object"]))
 
-    if len(li.find_all("a", class_=username_identifier)) != 0:
-        username_tag = li.find_all("a", class_=username_identifier)[0]
-        new_review_dict["user_name"] = username_tag.text
-
-    if len(li.find_all("div", class_=rating_identifier)) != 0:
-        rating_tag = li.find_all("div", class_=rating_identifier)[0]
-        new_review_dict["rating"] = formatted_rating(rating_tag.attrs["title"])
+    if len(li.find_all("span", class_=date_identifier)) != 0:
+        date_tag = li.find_all("span", class_=date_identifier)[0]
+        new_review_list.append(date_tag.text.strip())
 
     if len(li.find_all("p")) != 0:
         feedback_tag = li.find_all("p")[0]
-        new_review_dict["review"] = feedback_tag.text
+        new_review_list.append(feedback_tag.text.strip())
+
+    if len(li.find_all("div", class_=rating_identifier)) != 0:
+        rating_tag = li.find_all("div", class_=rating_identifier)[0]
+        new_review_list.append(formatted_rating(rating_tag.attrs["title"]))
 
     if len(li.find_all("a", class_=useful_count_identifier)) != 0:
         useful_count_tree = li.find_all("a", class_=useful_count_identifier)[0]
-        new_review_dict["useful_votes"] = formatted_count(useful_count_tree.find_all("span", class_="count")[0].text)
+        new_review_list.append(formatted_count(useful_count_tree.find_all("span", class_="count")[0].text))
 
     if len(li.find_all("a", class_=funny_count_identifier)) != 0:
         funny_count_tree = li.find_all("a", class_=funny_count_identifier)[0]
-        new_review_dict["funny_votes"] = formatted_count(funny_count_tree.find_all("span", class_="count")[0].text)
+        new_review_list.append(formatted_count(funny_count_tree.find_all("span", class_="count")[0].text))
 
     if len(li.find_all("a", class_=cool_count_identifier)) != 0:
         cool_count_tree = li.find_all("a", class_=cool_count_identifier)[0]
-        new_review_dict["cool_votes"] = formatted_count(cool_count_tree.find_all("span", class_="count")[0].text)
+        new_review_list.append(formatted_count(cool_count_tree.find_all("span", class_="count")[0].text))
 
-    rev_data_list.append(new_review_dict)
+    rev_data_list.append(new_review_list)
 
 
 def generate_author_list(li):
+
     new_author_list = []
     if len(li.find_all("div", class_=userid_identifier)) != 0:
         userid_tag = li.find_all("div", class_=userid_identifier)[0]
@@ -241,9 +255,21 @@ def generate_author_list(li):
     author_data_list.append(new_author_list)
 
 
+def generate_author_csv():
+    with open(author_csv_path, "w") as output:
+        writer = csv.writer(output, lineterminator='\n')
+        writer.writerows(author_data_list[1:])
+
+
+def generate_review_csv():
+    with open(review_csv_path, "w") as output:
+        writer = csv.writer(output, lineterminator='\n')
+        writer.writerows(rev_data_list[1:])
+
+
 print("{0} {1}".format("\n\n", get_reviews_for_restaurant("https://www.yelp.com/biz/meli-cafe-and-juice-bar-chicago")))
 
-get_reviews_for_restaurant("https://www.yelp.com/biz/big-and-littles-restaurant-chicago-3")
-for x in restaurant_data_list:
-    for key, value in x.items():
-        print(key, value)
+#get_reviews_for_restaurant("https://www.yelp.com/biz/big-and-littles-restaurant-chicago-3")
+# for x in restaurant_data_list:
+#     for key, value in x.items():
+#         print(key, value)
